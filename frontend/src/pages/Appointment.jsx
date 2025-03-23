dcdb044d9896166f33956bac64ec27bc7bd6a342
Sarthak Kumbhar <sarthakkumbhar43@gamil.com>
2025-03-23 16:43:23 +0530

    Improve the UI of Appoitment booking

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,6 +1,13 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import AuthContext from "../context/AuthContext.jsx";
 import { useNavigate } from "react-router-dom";
+import TextField from "@mui/material/TextField";
+import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { StaticTimePicker } from '@mui/x-date-pickers/StaticTimePicker';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 
 const Appointment = () => {
   const { user, bookAppointment } = useContext(AuthContext);
@@ -8,67 +15,74 @@ const Appointment = () => {
   const [date, setDate] = useState("");
   const [time, setTime] = useState("");
   const [reason, setreason] = useState("");
+  const [width, setWidth] = useState(window.innerWidth);
   const navigate = useNavigate();
+
+
+  
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     await bookAppointment(doctorId, date, time, reason, user?._id);
     alert("Appointment booked successfully!");
-    navigate("/list-appointments",{ replace: true })
+    navigate("/list-appointments", { replace: true });
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
-      <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
-        <h2 className="text-2xl font-semibold text-blue-600 text-center mb-4">
-          Book Appointment
-        </h2>
-        <form onSubmit={handleSubmit} className="space-y-4">
+    <div className="min-h-screen p-8">
+      <div className="p-4">
+        <h2 className="text-2xl text-[#088395] text-center font-semibold mb-4">Book Appointment</h2>
+        <form onSubmit={handleSubmit} className="space-y-4 mt-10">
           <div>
-            <label className="block text-gray-700">Doctor ID</label>
-            <input
-              type="text"
-              placeholder="Find Doctor and Enter ID"
-              value={doctorId}
+            <TextField
+              id="standard-basic"
               onChange={(e) => setDoctorId(e.target.value)}
-              className="w-full p-2 border rounded-md"
+              value={doctorId}
+              label="Doctor ID"
+              variant="outlined"
+              className="w-full"
               required
             />
           </div>
-          <div>
-            <label className="block text-gray-700">Date</label>
-            <input
-              type="date"
-              value={date}
-              onChange={(e) => setDate(e.target.value)}
-              className="w-full p-2 border rounded-md"
-              required
-            />
+          <div className="mt-8">
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DemoContainer components={["DatePicker"]}>
+                <DatePicker
+                  label="Select Date"
+                  onChange={(e) => setDate(e.target.value)}
+                  className="w-full"
+                  required
+                />
+              </DemoContainer>
+            </LocalizationProvider>
           </div>
-          <div>
-            <label className="block text-gray-700">Time</label>
-            <input
-              type="time"
-              value={time}
-              onChange={(e) => setTime(e.target.value)}
-              className="w-full p-2 border rounded-md"
-              required
-            />
+          <div className="mt-8">
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DemoItem >
+                {width < 700 ? <TimePicker label="Time" name="startTime" onChange={(e) => setTime(e.target.value)}/> : <StaticTimePicker orientation="landscape" className="w-[500px]"/>}
+                
+                
+              </DemoItem>
+            </LocalizationProvider>
           </div>
-          <div>
-            <label className="block text-gray-700">reason</label>
-            <input
-              type="text"
-              placeholder="Enter reason"
+          <div className="mt-8">
+            <TextField
+              id="standard-basic"
+              label="Reason"
+              variant="outlined"
               value={reason}
               onChange={(e) => setreason(e.target.value)}
-              className="w-full p-2 border rounded-md"
-              required
+              className="w-full"
             />
           </div>
           <button
             type="submit"
-            className="w-full bg-blue-600 text-white p-2 rounded-md hover:bg-blue-700"
+            className="w-full mt-8 bg-[#071952] text-white p-2 rounded-md cursor-pointer"
           >
             Book Appointment
           </button>
